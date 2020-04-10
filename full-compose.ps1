f50b49ce3a60e01cbe6d51d725123c9cc501e8ae
Alejandro <Alejandro@host.docker.internal>
2020-04-11 01:26:31 +0200

    env file

--- full-compose.ps1
+++ full-compose.ps1
@@ -1,3 +1,3 @@
 docker stack rm demo
-docker-compose --file docker-compose.yml --env-file dev.env build --force-rm --pull --q --parallel --no-cache
+docker-compose --file docker-compose.yml --env-file dev.env build --force-rm --pull --q --parallel
 docker stack deploy -c ./docker-compose.yml demo
